d67cf0e9b0de8588cac8d1beae0bf250c8e36fa0
g-e-h-z <gehz250696@gmail.com>
2019-09-05 14:53:45 -0500

    sistema de rutas principales y de paginas funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraphicsComponent } from './pages/graphics/graphics.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 import { NoPageComponent } from './shared/no-page/no-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graphics', component: GraphicsComponent },
-      { path: '', pathMatch: 'full', redirectTo: 'dashboard' }
-    ]
-  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'not-found', component: NoPageComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'not-found' }
+  { path: '**', component: NoPageComponent }
 ];
 
 @NgModule({
